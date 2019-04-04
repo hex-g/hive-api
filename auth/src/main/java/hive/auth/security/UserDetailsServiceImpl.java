ba97209bb28e45b21cef98c914c1cdca9785762f
Victor Hirumitsu <vmhirumitsu@gmail.com>
2019-04-04 20:00:29 -0300

    Add repository

--- auth/src/main/java/hive/auth/security/UserDetailsServiceImpl.java
+++ auth/src/main/java/hive/auth/security/UserDetailsServiceImpl.java
@@ -1,23 +1,38 @@
 package hive.auth.security;
 
+import hive.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+  // -----------------------------------------------------------------------------------
+  // hardcode some users
+  private final BCryptPasswordEncoder encoder;
+  // -----------------------------------------------------------------------------------
+
+  private final UserRepository userRepository;
+
   @Autowired
-  private RestTemplate restTemplate;
+  public UserDetailsServiceImpl(UserRepository userRepository, BCryptPasswordEncoder encoder) {
+    this.userRepository = userRepository;
+
+    // hardcode some users
+    userRepository.save(new hive.entity.user.User("admin", encoder.encode("admin"), 0, 0));
+    userRepository.save(new hive.entity.user.User("ped", encoder.encode("ped"), 0, 0));
+    userRepository.save(new hive.entity.user.User("stu", encoder.encode("stu"), 0, 0));
+    this.encoder = encoder;
+  }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    var hiveUser =
-        restTemplate.getForObject("http://repository-service/user/" + username, hive.common.entity.User.class);
+    var hiveUser = userRepository.findByUsername(username);
 
     if (hiveUser == null) {
       throw new UsernameNotFoundException("Username: " + username + " not found");
---
+++ auth/src/test/java/hive/auth/repository/UserRepositoryTest.java
@@ -0,0 +1,13 @@
+package hive.auth.repository;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserRepositoryTest {
+}
