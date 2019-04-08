d8dcaf79800eaeb1c9c27c5c41200ec6ef3e4318
Victor Hirumitsu <vmhirumitsu@gmail.com>
2019-04-08 01:25:42 -0300

    Fix whenNoteIsRetrieved tests

--- kirby/src/main/java/hive/kirby/controller/NoteController.java
+++ kirby/src/main/java/hive/kirby/controller/NoteController.java
@@ -1,31 +1,59 @@
 package hive.kirby.controller;
 
+import hive.common.security.HiveHeaders;
 import hive.kirby.entity.Note;
 import hive.kirby.exception.InvalidPathException;
 import hive.kirby.exception.NoteNotFoundException;
+import hive.kirby.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/note")
 public class NoteController {
-  static String rootDir = "kirby-storage";
+  @Value("${hive.kirby.storage-directory}")
+  private String rootDir;
 
-  @GetMapping()
-  public Note serveNoteByPath(@RequestParam final String path) {
+  private final UserRepository userRepository;
+
+  @Autowired
+  public NoteController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping
+  public Note serveNote(
+      @RequestHeader(name = HiveHeaders.AUTHENTICATED_USER_NAME_HEADER) final String username,
+      @RequestParam final String path
+  ) {
     if (path.contains("..") || path.endsWith("/")) {
       throw new InvalidPathException();
     }
 
-    try (var stream = new FileInputStream(path)) {
+    var user = userRepository.findByUsername(username);
+
+    try (var stream = new FileInputStream(Paths.get(rootDir, user.getId().toString(), path).toFile())) {
       return new Note(path, new String(stream.readAllBytes()));
     } catch (FileNotFoundException e) {
       throw new NoteNotFoundException();
     } catch (IOException e) {
       throw new RuntimeException();
+    }
+  }
+
+  // TODO
+  @PostMapping
+  public void saveNote(@RequestBody final Note note) {
+    final var path = note.getPath();
+
+    if (path.contains("..") || path.endsWith("/")) {
+      throw new InvalidPathException();
     }
   }
 }
--- kirby/src/test/java/hive/kirby/controller/NoteControllerTest.java
+++ kirby/src/test/java/hive/kirby/controller/NoteControllerTest.java
@@ -2,15 +2,23 @@ package hive.kirby.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hive.common.security.HiveHeaders;
+import hive.entity.user.User;
 import hive.kirby.entity.Note;
+import hive.kirby.repository.UserRepository;
 import org.apache.commons.lang.RandomStringUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,21 +27,50 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(NoteController.class)
+@SpringBootTest
 public class NoteControllerTest {
-  @Autowired
+  @Value("${hive.kirby.storage-directory}")
+  private String rootDir;
+
   private MockMvc mockMvc;
+
+  @Mock
+  private UserRepository userRepository;
+  @Mock
+  private User user;
+  private String username = RandomStringUtils.randomAlphabetic(8);
+  private Integer userId = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+
+    when(user.getId()).thenReturn(userId);
+    when(userRepository.findByUsername(username)).thenReturn(user);
+
+    var noteController = new NoteController(userRepository);
+
+    ReflectionTestUtils.setField(noteController, "rootDir", rootDir);
+
+    mockMvc = MockMvcBuilders.standaloneSetup(noteController).build();
+  }
 
   @Test
   public void givenPathContainsParentDirectoryReference_whenNoteIsRetrieved_then400IsReceived() throws Exception {
     mockMvc
-        .perform(get("/note").param("path", "..").contentType(MediaType.APPLICATION_JSON))
+        .perform(
+            get("/note")
+                .header(HiveHeaders.AUTHENTICATED_USER_NAME_HEADER, username)
+                .param("path", "..")
+        )
         .andExpect(status().isBadRequest())
         .andExpect(status().reason("Invalid path"));
   }
@@ -41,7 +78,11 @@ public class NoteControllerTest {
   @Test
   public void givenPathEndsWithSlash_whenNoteIsRetrieved_then400IsReceived() throws Exception {
     mockMvc
-        .perform(get("/note").param("path", "/").contentType(MediaType.APPLICATION_JSON))
+        .perform(
+            get("/note")
+                .header(HiveHeaders.AUTHENTICATED_USER_NAME_HEADER, username)
+                .param("path", "/")
+        )
         .andExpect(status().isBadRequest())
         .andExpect(status().reason("Invalid path"));
   }
@@ -49,60 +90,66 @@ public class NoteControllerTest {
   @Test
   public void givenPathIsValidAndNoteDoesNotExist_whenNoteIsRetrieved_then404IsReceived() throws Exception {
     mockMvc
-        .perform(get("/note").param("path", "a/valid/path").contentType(MediaType.APPLICATION_JSON))
+        .perform(
+            get("/note")
+                .header(HiveHeaders.AUTHENTICATED_USER_NAME_HEADER, username)
+                .param("path", "a/valid/path")
+        )
         .andExpect(status().isNotFound())
         .andExpect(status().reason("Note not found"));
   }
 
   @Test
   public void givenPathIsValidAndNoteExists_whenNoteIsRetrieved_then200IsReceived() throws Exception {
-    var userId = RandomStringUtils.randomNumeric(4);
-    var dirs = NoteController.rootDir + "/" + userId + "/a/valid/path/to/a";
-    var path = dirs + "/file.md";
+    var id = userId;
+    var dirs = rootDir + "/" + id + "/a/valid/path/to/a";
+    var fullPath = dirs + "/file.md";
+    var path = "a/valid/path/to/a/file.md";
 
     try {
       Files.createDirectories(Paths.get(dirs));
-      Files.createFile(Paths.get(path));
+      Files.createFile(Paths.get(fullPath));
 
       mockMvc
           .perform(
               get("/note")
-                  .header(HiveHeaders.AUTHENTICATED_USER_NAME_HEADER, userId)
+                  .header(HiveHeaders.AUTHENTICATED_USER_NAME_HEADER, username)
                   .param("path", path)
-                  .contentType(MediaType.APPLICATION_JSON))
+          )
           .andExpect(status().isOk());
     } finally {
       try {
-        Files.walk(Paths.get(NoteController.rootDir))
+        Files.walk(Paths.get(rootDir, id.toString()))
             .sorted(Comparator.reverseOrder())
             .map(Path::toFile)
             .forEach(File::delete);
       } catch (IOException e) {
-        System.err.println("Created files could not be deleted. Root directory name: " + userId + ".");
+        System.err.println("Created files could not be deleted. Root directory name: " + id + ".");
       }
     }
   }
 
   @Test
   public void givenPathIsValidAndNoteExists_whenNoteIsRetrieved_thenRetrievedResourceIsCorrect() throws Exception {
-    var userId = RandomStringUtils.randomNumeric(4);
-    var dirs = NoteController.rootDir + "/" + userId + "/a/valid/path/to/a";
-    var path = dirs + "/file.md";
-    var fileContent = RandomStringUtils.randomAscii(1024);
+    var id = userId;
+    var dirs = rootDir + "/" + id + "/a/valid/path/to/a";
+    var fullPath = dirs + "/file.md";
+    var path = "a/valid/path/to/a/file.md";
+    var fileContent = RandomStringUtils.randomAscii(2048);
 
     try {
       Files.createDirectories(Paths.get(dirs));
-      Files.createFile(Paths.get(path));
-      try (var fileOutputStream = new FileOutputStream(path)) {
+      Files.createFile(Paths.get(fullPath));
+      try (var fileOutputStream = new FileOutputStream(fullPath)) {
         fileOutputStream.write(fileContent.getBytes());
       } catch (IOException e) {
-        System.err.println("Could not write to created file. File: " + path);
+        System.err.println("Could not write to created file. File: " + fullPath);
       }
 
       var result = mockMvc
           .perform(
               get("/note")
-                  .header(HiveHeaders.AUTHENTICATED_USER_NAME_HEADER, userId)
+                  .header(HiveHeaders.AUTHENTICATED_USER_NAME_HEADER, username)
                   .param("path", path)
                   .contentType(MediaType.APPLICATION_JSON))
           .andReturn();
@@ -113,12 +160,12 @@ public class NoteControllerTest {
       assertEquals(fileContent, actual.getContent());
     } finally {
       try {
-        Files.walk(Paths.get(NoteController.rootDir))
+        Files.walk(Paths.get(rootDir, id.toString()))
             .sorted(Comparator.reverseOrder())
             .map(Path::toFile)
             .forEach(File::delete);
       } catch (IOException e) {
-        System.err.println("Created files could not be deleted. Root directory name: " + userId + ".");
+        System.err.println("Created files could not be deleted. Root directory name: " + id + ".");
       }
     }
   }
