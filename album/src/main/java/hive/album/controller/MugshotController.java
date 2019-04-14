9f3baec7b430ee28adb8394784527a78bbf095ff
germano <germanosp11@hotmail.com>
2019-04-14 17:26:26 -0300

    Added Delete Method. Post and Gets uses username as folder name now.

--- album/src/main/java/hive/album/controller/MugshotController.java
+++ album/src/main/java/hive/album/controller/MugshotController.java
@@ -1,10 +1,10 @@
 package hive.album.controller;
 
-import hive.album.entity.ProfileImage;
-import hive.album.exception.ImageNotFound;
+import hive.album.exception.UserNotFoundException;
 import hive.album.repository.UserRepository;
 import hive.album.storage.ImageStorer;
 import hive.common.security.HiveHeaders;
+import hive.entity.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -15,48 +15,65 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.core.io.Resource;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
-
 @RestController
-@RequestMapping("/")
+@RequestMapping("/smugshot")
 public class MugshotController {
-  private final UserRepository userRepository;
+
   @Value("${hive.mugshot.image-directory-path}")
   private String rootDir;
-  @Autowired
-  private ImageStorer imageStorer;
-  @ResponseStatus(code = HttpStatus.I_AM_A_TEAPOT, reason = "The server is working")
-  @GetMapping("/hey")
-  public void hey() {}
-  @GetMapping("/dir")
-  public String getRootDirectory() {
+
+  private final ImageStorer imageStorer;
+  private final UserRepository userRepository;
+
+  @Autowired public MugshotController(UserRepository userRepository,ImageStorer imageStorer) {
+    this.imageStorer=imageStorer;
+    this.userRepository = userRepository;
+    userRepository.save(new User("TESTE","123",0,0));
+    userRepository.save(new User("GERMANO","123",0,0));
+    userRepository.save(new User("JAVA","123",0,0));
+  }
+
+  @ResponseStatus(code = HttpStatus.I_AM_A_TEAPOT, reason = "The server is working and have an user")
+  @GetMapping("/hey") public void hey(){}
+
+  @GetMapping("/dir") public String getRootDirectory() {
     return rootDir;
   }
-  @Autowired
-  public MugshotController(UserRepository userRepository) {
-    this.userRepository = userRepository;
+  @GetMapping("/user") public String getUser
+      (@RequestHeader(name = HiveHeaders.AUTHENTICATED_USER_NAME_HEADER) final String username)
+  {
+    var user=userRepository.findByUsername(username);
+    return user.getUsername();
   }
   //tratar multiplos arquivos excecao
   //tratar replace de imagens
   //tratar replace de imagem não proprietaria
   //tratar renomeacao de imagem
-  @PostMapping("smugshot")
-  public String sendImageProfile
-      ( @RequestParam("image") MultipartFile insertedImage,
-        @RequestHeader(name = HiveHeaders.AUTHENTICATED_USER_NAME_HEADER) final String username) {
-    return imageStorer.StoreImageProfile(insertedImage);
-  }
-  @GetMapping(value="smugshot/{profileImageName:.+}",produces = MediaType.IMAGE_JPEG_VALUE)
-  public ResponseEntity<Resource> searchProfileImage
-      (@RequestHeader(name = HiveHeaders.AUTHENTICATED_USER_NAME_HEADER) final String username,
-       @PathVariable String profileImageName){
+  @PostMapping
+  public String sendImageProfile(@RequestParam("image") MultipartFile insertedImage, @RequestHeader(name = HiveHeaders.AUTHENTICATED_USER_NAME_HEADER) final String username) {
     var user = userRepository.findByUsername(username);
-    Resource file = imageStorer.loadImage(profileImageName);
-    return ResponseEntity.ok().header(
-        HttpHeaders.CONTENT_DISPOSITION,
-        "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    if(user==null){
+      throw new UserNotFoundException();
+    }
+    return imageStorer.StoreImageProfile(username,insertedImage);
+  }
+
+  @GetMapping(value = "/{profileImageName:.+}", produces = MediaType.IMAGE_JPEG_VALUE)
+  public ResponseEntity<Resource> searchProfileImage(@RequestHeader(name = HiveHeaders.AUTHENTICATED_USER_NAME_HEADER) final String username, @PathVariable String profileImageName) {
+    var user = userRepository.findByUsername(username);
+    if(user==null){
+      throw new UserNotFoundException();
+    }
+    Resource file = imageStorer.loadImage(username,profileImageName);
+    return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+  }
+
+  @DeleteMapping
+  public void deleteProfileImage(@RequestHeader(name = HiveHeaders.AUTHENTICATED_USER_NAME_HEADER) final String username) {
+    var user = userRepository.findByUsername(username);
+    if(user==null){
+      throw new UserNotFoundException();
+    }
+    imageStorer.deleteAllUserImages(username);
   }
 }
--- album/src/main/java/hive/album/storage/ImageStorer.java
+++ album/src/main/java/hive/album/storage/ImageStorer.java
@@ -1,6 +1,7 @@
 package hive.album.storage;
 
 import hive.album.exception.ImageAlreadyExistException;
+import hive.album.exception.ImageNotFound;
 import hive.album.exception.ImageProfileException;
 import hive.album.exception.InvalidPathException;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,14 +21,14 @@ import java.nio.file.Paths;
 public class ImageStorer {
   @Value("${hive.mugshot.image-directory-path}")
   private String rootDir;
-  private Path resolvePathFullUrlWith(String filename) {
-    return Paths.get(rootDir).resolve(filename);
-  }
-  public String StoreImageProfile(MultipartFile insertedImage){
-    this.createDirectoryIfNotExist();
+
+  public String StoreImageProfile(String userDirectoryName,MultipartFile insertedImage){
+    this.createDirectoryIfNotExist(userDirectoryName);
     try {
-      Files.copy( insertedImage.getInputStream(),
-                  Paths.get(rootDir).resolve(insertedImage.getOriginalFilename()));
+      Files.copy(
+          insertedImage.getInputStream(),
+          resolvePathFullUrlWith(userDirectoryName,insertedImage.getOriginalFilename())
+      );
     }catch(FileAlreadyExistsException e){
       throw new ImageAlreadyExistException();
     }
@@ -36,8 +37,8 @@ public class ImageStorer {
     }
     return "Image Stored in Success";
   }
-  private void createDirectoryIfNotExist(){
-    Path parentDir = Paths.get(rootDir);
+  private void createDirectoryIfNotExist(String userDirectoryPath){
+    Path parentDir = Paths.get(rootDir,userDirectoryPath);
     if (!Files.exists(parentDir)) {
       try {
         Files.createDirectories(parentDir);
@@ -46,17 +47,32 @@ public class ImageStorer {
       }
     }
   }
-  public Resource loadImage(String ImageName) {
+
+  public Resource loadImage(String userDirectoryName,String ImageName) {
     try {
-      Path file = resolvePathFullUrlWith(ImageName);
+      Path file = resolvePathFullUrlWith(userDirectoryName,ImageName);
       Resource resource = new UrlResource(file.toUri());
       if (resource.exists() || resource.isReadable()) {
         return resource;
+      }else{
+        throw new ImageNotFound();
       }
     } catch (MalformedURLException e) {
       e.printStackTrace();
       throw new InvalidPathException();
     }
-    return null;
+  }
+  private Path resolvePathFullUrlWith(String userDirectoryName,String filename) {
+    return Paths.get(rootDir).resolve(userDirectoryName).resolve(filename);
+  }
+
+  public void deleteAllUserImages(String userDirectoryName) {
+    Path parentDir = Paths.get(rootDir,userDirectoryName);
+    try {
+      Files.deleteIfExists(parentDir);
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException("Not capable to delete the directory.\n"+e);
+    }
   }
 }
