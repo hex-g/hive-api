2371ec6656024f087f1382a58540f90ce4bb74c8
germano <germanosp11@hotmail.com>
2019-04-20 01:12:39 -0300

    Alteration: Only multiPartFile upload test is working properly for now.

--- album/src/main/java/hive/album/storage/ImageStorer.java
+++ album/src/main/java/hive/album/storage/ImageStorer.java
@@ -2,7 +2,6 @@ package hive.album.storage;
 
 import hive.album.exception.ImageAlreadyExistException;
 import hive.album.exception.ImageNotFound;
-import hive.album.exception.ImageProfileException;
 import hive.album.exception.InvalidPathException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -10,32 +9,27 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 @Service
 public class ImageStorer {
   @Value("${hive.mugshot.image-directory-path}")
   private String rootDir;
 
-  public String StoreImageProfile(String userDirectoryName,MultipartFile insertedImage){
+  public void StoreImageProfile(String userDirectoryName,MultipartFile insertedImage,String imageStoredName){
     this.createDirectoryIfNotExist(userDirectoryName);
     try {
-      Files.copy(
-          insertedImage.getInputStream(),
-          resolvePathFullUrlWith(userDirectoryName,insertedImage.getOriginalFilename())
-      );
+      var buff=ImageUtils.resizeImageToSquare(ImageIO.read(insertedImage.getInputStream()));
+      ImageIO.write(buff,"png", resolveRootPathFullUrlWith(userDirectoryName,imageStoredName).toFile());
     }catch(FileAlreadyExistsException e){
       throw new ImageAlreadyExistException();
     }
     catch (IOException e) {
       throw new RuntimeException(e);
     }
-    return "Image Stored in Success";
   }
   private void createDirectoryIfNotExist(String userDirectoryPath){
     Path parentDir = Paths.get(rootDir,userDirectoryPath);
@@ -50,7 +44,7 @@ public class ImageStorer {
 
   public Resource loadImage(String userDirectoryName,String ImageName) {
     try {
-      Path file = resolvePathFullUrlWith(userDirectoryName,ImageName);
+      Path file = resolveRootPathFullUrlWith(userDirectoryName,ImageName);
       Resource resource = new UrlResource(file.toUri());
       if (resource.exists() || resource.isReadable()) {
         return resource;
@@ -62,12 +56,12 @@ public class ImageStorer {
       throw new InvalidPathException();
     }
   }
-  private Path resolvePathFullUrlWith(String userDirectoryName,String filename) {
+  private Path resolveRootPathFullUrlWith(String userDirectoryName, String filename) {
     return Paths.get(rootDir).resolve(userDirectoryName).resolve(filename);
   }
 
-  public void deleteAllUserImages(String userDirectoryName) {
-    Path parentDir = Paths.get(rootDir,userDirectoryName);
+  public void deleteAllUserImages(String userDirectoryName,String ImageName) {
+    Path parentDir = resolveRootPathFullUrlWith(userDirectoryName,ImageName);
     try {
       Files.deleteIfExists(parentDir);
     } catch (IOException e) {
