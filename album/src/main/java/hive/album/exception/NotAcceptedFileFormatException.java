5ee2aa5f5cd1f02f39c284c6bb249c29166385dd
germano <germanosp11@hotmail.com>
2019-04-21 23:27:57 -0300

    New exception for 415(NotSupportedMediaType) http response.

---
+++ album/src/main/java/hive/album/exception/NotAcceptedFileFormatException.java
@@ -0,0 +1,7 @@
+package hive.album.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNSUPPORTED_MEDIA_TYPE, reason = "Media type unsupported")
+public class NotAcceptedFileFormatException extends RuntimeException{
+}
