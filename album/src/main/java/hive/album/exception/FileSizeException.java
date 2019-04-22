7e9621a5df0ce14d248d75d1eb8da28db2241620
germano <germanosp11@hotmail.com>
2019-04-21 23:58:06 -0300

    new Exception for 413(PayloadTooLarge) http response

--- album/src/main/java/hive/album/exception/FileSizeException.java
+++ album/src/main/java/hive/album/exception/FileSizeException.java
@@ -2,6 +2,6 @@ package hive.album.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Invalid size of file")
+@ResponseStatus(code = HttpStatus.PAYLOAD_TOO_LARGE,reason = "Invalid size of file")
 public class FileSizeException extends RuntimeException{
 }
