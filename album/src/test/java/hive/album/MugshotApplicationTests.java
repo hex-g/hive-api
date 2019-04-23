ccaecf7e9c148638b255beed58d153c35cf0586b
germano <germanosp11@hotmail.com>
2019-04-22 23:35:29 -0300

    correction in typo

--- album/src/test/java/hive/album/MugshotApplicationTests.java
+++ album/src/test/java/hive/album/MugshotApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SmugshotApplicationTests {
+public class MugshotApplicationTests {
 
   @Test
   public void contextLoads() {
