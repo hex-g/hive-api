33cb12a4548b6cf4db4f95cb45162cb02b1fdf73
germano <germanosp11@hotmail.com>
2019-04-26 00:27:44 -0300

    New tests and mocks.

--- album/src/test/java/hive/album/controller/MugshotControllerTest.java
+++ album/src/test/java/hive/album/controller/MugshotControllerTest.java
@@ -1,10 +1,12 @@
 package hive.album.controller;
 
 import hive.album.exception.ImageNotFound;
+import hive.album.exception.NotAcceptedFileFormatException;
 import hive.album.repository.UserRepository;
 import hive.album.storage.ImageStorer;
 import hive.entity.user.User;
 import org.apache.commons.lang.RandomStringUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +15,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -25,6 +30,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -43,20 +49,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MugshotControllerTest {
-  @Value("${hive.mugshot.image-directory-path}")
-  private String rootDir;
+
   private MockMvc mockMvc;
-  private String imageName="ProfileImage.jpg";
   private String validDirectoryName;
-  @Mock
-  private UserRepository userRepository;
-  @Autowired
-  private ImageStorer imageStorer;
-  @Mock
-  private User user;
   private String username = RandomStringUtils.randomAlphabetic(8);
   private Integer userId = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
   private MockMultipartFile multipartFile;
+  @Value("${hive.mugshot.image-directory-path}")
+  private String rootDir;
+  @Value("${hive.mugshot.profile-image-name}")
+  private String imageName;
+  @Mock
+  private UserRepository userRepository;
+  @Mock
+  private ImageStorer imageStorer;
+  @Mock
+  private User user;
+
+  private Resource createImageForTest(String directoryName) throws Exception{
+    var file=new File(directoryName + "/ProfileImage.jpg");
+    ImageIO.write(new BufferedImage(512,512,BufferedImage.TYPE_INT_RGB),"jpg",file);
+    return new UrlResource(file.toURI());
+  }
+
+  private void createDirectoryForTest(String directoryName) throws Exception{
+    Files.createDirectories(Paths.get(directoryName));
+  }
 
   @Before
   public void setup() {
@@ -65,40 +83,26 @@ public class MugshotControllerTest {
     when(userRepository.findByUsername(username)).thenReturn(user);
     var mugshotController = new MugshotController(imageStorer,userRepository);
     ReflectionTestUtils.setField(mugshotController, "rootDir", rootDir);
+    ReflectionTestUtils.setField(mugshotController, "imageName", imageName);
     mockMvc = MockMvcBuilders.standaloneSetup(mugshotController).build();
     validDirectoryName = (rootDir + "/" + user.getId() + "/");
   }
 
-  private void createImageForTest(String directoryName) throws Exception{
-    var file=new File(directoryName + "/ProfileImage.jpg");
-    ImageIO.write(new BufferedImage(512,512,BufferedImage.TYPE_INT_RGB),"jpg",file);
-  }
-  private void createDirectoryForTest(String directoryName) throws Exception{
-      Files.createDirectories(Paths.get(directoryName));
-  }
-  private void deleteTestDirectory(String directoryName) throws Exception{
-      Files.delete(Paths.get(directoryName));
-  }
-  private void deleteTestImage(String directoryName) throws Exception{
-    Files.delete(Paths.get(directoryName+"/ProfileImage.jpg"));
-  }
   //SUCCESS:
   @Test
   public void givenValidImage_WhenImageRetrieved_then200andJpegImageTypeIsReturned() throws Exception{
     createDirectoryForTest(validDirectoryName);
-    createImageForTest(validDirectoryName);
-    try {
+    var resourceImage=createImageForTest(validDirectoryName);
+    given(imageStorer.loadImage(userId.toString(),imageName))
+        .willReturn(resourceImage);
       mockMvc.perform(
           get("/mugshot")
           .header("authenticated-user-name", username))
           .andExpect(status().isOk())
           .andExpect(content().contentType(MediaType.IMAGE_JPEG))
           .andDo(print());
-    }finally {
-      deleteTestImage(validDirectoryName);
-      deleteTestDirectory(validDirectoryName);
-    }
   }
+
   @Test
   public void givenValidImage_WhenImageUploaded_then200isReturned() throws Exception{
     multipartFile = new MockMultipartFile("image", "Profile Image.jpg", MediaType.IMAGE_JPEG_VALUE, "Spring Framework".getBytes());
@@ -106,58 +110,56 @@ public class MugshotControllerTest {
         multipart("/mugshot").file(multipartFile).header("authenticated-user-name",username))
         .andExpect(status().isOk())
         .andDo(print());
-    deleteTestImage(validDirectoryName);
-    deleteTestDirectory(validDirectoryName);
   }
+
   @Test
   public void givenValidImage_WhenImageDeleted_then204isReturned() throws Exception{
     mockMvc.perform(
         delete("/mugshot").header("authenticated-user-name",username)
     ).andExpect(status().isNoContent());
   }
+
   //ERRORS:
   @Test
   public void givenFileNotFound_WhenImageRetrieved_then404isReturned() throws Exception{
+    given(imageStorer.loadImage(userId.toString(),imageName)).willThrow(new ImageNotFound());
     createDirectoryForTest(validDirectoryName);
-    try {
-      mockMvc.perform(
-          get("/mugshot")
-              .header("authenticated-user-name", username))
-          .andExpect(status().isNotFound())
-          .andDo(print());
-    }finally {
-      deleteTestDirectory(validDirectoryName);
-    }
+    mockMvc.perform(
+        get("/mugshot")
+        .header("authenticated-user-name", username))
+        .andExpect(status().isNotFound())
+        .andDo(print());
   }
+
   @Test
   public void givenUnsupportedMediaType_WhenImageUploaded_then415isReturned()throws Exception{
       BufferedImage originalImage = new BufferedImage(512, 512, BufferedImage.TYPE_INT_RGB);
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      ImageIO.write(originalImage, "jpg", baos);
-      multipartFile = new MockMultipartFile("image", "ProfileImage.wmv", MediaType.APPLICATION_PDF_VALUE, baos.toByteArray());
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      ImageIO.write(originalImage, "jpg", byteArrayOutputStream);
+      multipartFile = new MockMultipartFile("image", "Unsupported.Extension.wmv", MediaType.APPLICATION_PDF_VALUE, byteArrayOutputStream.toByteArray());
       mockMvc.perform(multipart("/mugshot")
           .file(multipartFile)
           .header("authenticated-user-name", username))
           .andExpect(status().isUnsupportedMediaType())
           .andDo(print());
   }
-  //falar com vitor sobre a gambiarra
-  @Test
-  public void givenFileTooLarge_WhenImageUploaded_then413isReturned() throws Exception{
-      byte[] moreThan1MB = new byte[1024 * 1024 * 2];
-      multipartFile = new MockMultipartFile("image", "ProfileImage.jpeg", MediaType.IMAGE_JPEG_VALUE, moreThan1MB);
-      mockMvc.perform(multipart("/mugshot")
-          .file(multipartFile).header("authenticated-user-name", username))
-          .andExpect(status().isPayloadTooLarge())
-          .andDo(print());
-  }
+
   @Test
   public void givenWrongBodyRequestKey_WhenImageUploaded_then400isReturned() throws Exception{
-      byte[] moreThan1MB = new byte[0];
-      multipartFile = new MockMultipartFile("wrongBodyKey", "ProfileImage.jpeg", MediaType.IMAGE_JPEG_VALUE, moreThan1MB);
+      byte[] EmptyFile = new byte[0];
+      multipartFile = new MockMultipartFile("wrongBodyKey", "ProfileImage.jpeg", MediaType.IMAGE_JPEG_VALUE, EmptyFile);
       mockMvc.perform(multipart("/mugshot")
           .file(multipartFile).header("authenticated-user-name", username))
           .andExpect(status().isBadRequest())
           .andDo(print());
   }
+
+  @After
+  public void deleteCreatedDirectory() throws IOException {
+    Path createdDirectoryPath=Paths.get(rootDir,userId.toString());
+    Path createdImagePath=createdDirectoryPath.resolve(imageName);
+    Files.deleteIfExists(createdImagePath);
+    Files.deleteIfExists(createdDirectoryPath);
+  }
+
 }
