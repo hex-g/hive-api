23213186d85c149d56213230f2519bd56cc60961
Samuel Levi <37668061+SamuelLevi@users.noreply.github.com>
2019-04-27 17:48:47 -0300

    Update EntityAlreadyExistsException.java

--- pokedex/src/main/java/hive/pokedex/exception/EntityAlreadyExistsException.java
+++ pokedex/src/main/java/hive/pokedex/exception/EntityAlreadyExistsException.java
@@ -3,6 +3,6 @@ package hive.pokedex.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Entity already registered, try again")
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Entity already registered")
 public class EntityAlreadyExistsException extends RuntimeException{
 }
