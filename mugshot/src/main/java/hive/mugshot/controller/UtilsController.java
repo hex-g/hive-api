e2b96eecfeaa9114bc92e959909fc5e8942de275
germano <germanosp11@hotmail.com>
2019-04-27 16:27:32 -0300

    Deleted test user, method "hey" and "searchSomeImage"

--- mugshot/src/main/java/hive/mugshot/controller/UtilsController.java
+++ mugshot/src/main/java/hive/mugshot/controller/UtilsController.java
@@ -22,21 +22,10 @@ public class UtilsController {
   @Value("${hive.mugshot.profile-image-name}")
   private String imageName;
 
-
-  @ResponseStatus(code = HttpStatus.OK, reason = "The server is working")
-  @GetMapping("/hey") public void hey(){}
   @Autowired
   public UtilsController(ImageStorer imageStorer,UserRepository userRepository){
     this.userRepository=userRepository;
     this.imageStorer=imageStorer;
-    userRepository.save(new User("Test_User","123",0,0));
-  }
-
-  @GetMapping(value = "searchImage/{profileImageName:.+}", produces = MediaType.IMAGE_JPEG_VALUE)
-  public ResponseEntity<Resource> searchSomeImage(@RequestHeader(name = HiveHeaders.AUTHENTICATED_USER_NAME_HEADER) final String username, @PathVariable String profileImageName) {
-    var userID=userRepository.findByUsername(username).getId().toString();
-    Resource file = imageStorer.loadImage(username,profileImageName);
-    return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
   }
 
   @ResponseStatus(code = HttpStatus.OK, reason = "Random image generated and successfully stored")
