7e7b2a79ee8f7c95a5b1c2c09bcc5504b833fd9c
Samuel Levi <37668061+SamuelLevi@users.noreply.github.com>
2019-04-27 17:49:24 -0300

    Update UsernameAlreadyExistsException.java

--- pokedex/src/main/java/hive/pokedex/exception/UsernameAlreadyExistsException.java
+++ pokedex/src/main/java/hive/pokedex/exception/UsernameAlreadyExistsException.java
@@ -3,6 +3,6 @@ package hive.pokedex.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Username already registered, try again")
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Username already registered")
 public class UsernameAlreadyExistsException extends RuntimeException{
 }
